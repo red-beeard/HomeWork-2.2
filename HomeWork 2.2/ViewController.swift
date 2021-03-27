7734c3f0e27cc38f4570b66932a2ece860f8f5e8
Alex Nifontov <nifon1998@yandex.ru>
2021-03-27 15:23:31 +0300

    Complete with constraint height view

--- HomeWork 2.2/ViewController.swift
+++ HomeWork 2.2/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var viewWithColor: UIView!
+    
+    @IBOutlet weak var valueOfRed: UILabel!
+    @IBOutlet weak var valueOfGreen: UILabel!
+    @IBOutlet weak var valueOfBlue: UILabel!
+    
+    @IBOutlet weak var sliderOfRedColor: UISlider!
+    @IBOutlet weak var sliderOfGreenColor: UISlider!
+    @IBOutlet weak var sliderOfBlueColor: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewWithColor.layer.cornerRadius = 10
+        
+        valueOfRed.text = String(format: "%3.2f", sliderOfRedColor.value)
+        valueOfGreen.text = String(format: "%3.2f", sliderOfGreenColor.value)
+        valueOfBlue.text = String(format: "%3.2f", sliderOfBlueColor.value)
+        
+        viewWithColor.backgroundColor = getCurrentColorFromSlider()
+
     }
-
-
+    
+    @IBAction func sliderRedColorAction() {
+        valueOfRed.text = String(format: "%.2f", sliderOfRedColor.value)
+        
+        viewWithColor.backgroundColor = getCurrentColorFromSlider()
+    }
+    
+    @IBAction func sliderGreenColorAction() {
+        valueOfGreen.text = String(format: "%.2f", sliderOfGreenColor.value)
+        
+        viewWithColor.backgroundColor = getCurrentColorFromSlider()
+    }
+    
+    @IBAction func sliderBlueColorAction() {
+        valueOfBlue.text = String(format: "%.2f", sliderOfBlueColor.value)
+        
+        viewWithColor.backgroundColor = getCurrentColorFromSlider()
+    }
+    
+    private func getCurrentColorFromSlider() -> UIColor {
+        UIColor(red: CGFloat(sliderOfRedColor.value),
+                green: CGFloat(sliderOfGreenColor.value),
+                blue: CGFloat(sliderOfBlueColor.value),
+                alpha: viewWithColor.alpha)
+    }
 }
 
