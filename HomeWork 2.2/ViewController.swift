fc0ae4f490b2a17073ef5049c56d3c1654f9a4d0
Alex Nifontov <nifon1998@yandex.ru>
2021-03-27 13:01:49 +0300

    Initial Commit

---
+++ HomeWork 2.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 2.2
+//
+//  Created by Red Beard on 27.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
